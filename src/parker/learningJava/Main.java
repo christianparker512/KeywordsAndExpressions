b4fda7263679a93e270ba5d99ba8648a9a709253
Christian Parker <christianparker512@gmail.com>
2021-09-02 09:41:15 -0500

    second score

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -20,17 +20,29 @@ public class Main {
                 " another " +
                 " still more.");
         //************************
-        Game
-        boolean gameOver = true;
-        int score = 5000;
-        int levelCompleted = 5;
-        int bonus = 100;
-        //the code block isn't necessary, really.
-        if (score <5000){
-            System.out.println("Your score was less 5000");
-        } else {
-            System.out.println("At least you had fun.");
-        }
-
+//        Game
+//        boolean gameOver = true;
+//        int score = 5000;
+//        int levelCompleted = 5;
+//        int bonus = 100;
+//        //the code block isn't necessary, really.
+//        if (score <5000){
+//            System.out.println("Your score was less 5000");
+//        } else {
+//            System.out.println("At least you had fun.");
+//        }
+//            if (gameOver == true){
+//                //if you create a variable in a codeblock you can't access that variable outside the codeblock. Scope
+//                int finalScore = score + (levelCompleted * bonus);
+//                System.out.println("Your final score was " + finalScore);
+//            }
+        boolean newGameOver = true;
+        int score = 1000;
+        int levelCompleted = 8;
+        int bonus = 200;
+            if (newGameOver){
+                int finalScore = score + levelCompleted * bonus);
+                System.out.println("Your final score was " + finalScore);
+            }
     }
 }
