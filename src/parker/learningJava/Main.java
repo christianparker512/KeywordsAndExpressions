5e56dfcd5bdd8a7d7a67d31a0abac3f2e43619cd
Christian Parker <christianparker512@gmail.com>
2021-09-02 09:05:32 -0500

    Initial commit

---
+++ src/parker/learningJava/Main.java
@@ -0,0 +1,23 @@
+package parker.learningJava;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //expressions are building blocks of all programs.
+        //a mile is equal to 1.609344 kilometers
+        //expressions are variables, values and operators
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+        if(highScore > 50){
+            System.out.println("You got thee high ");
+        }
+
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("this is a test");
+        System.out.println("This is " +
+                " another " +
+                " still more.");
+    }
+}
